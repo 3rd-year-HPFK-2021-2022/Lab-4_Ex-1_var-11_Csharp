14deeec81178096550944ceafed361a56c6be802
Vlad <78665125+Bambalela@users.noreply.github.com>
2021-10-17 21:52:40 +0300

    fix

--- Lab-4_Ex-1_var-11_Csharp/Program.cs
+++ Lab-4_Ex-1_var-11_Csharp/Program.cs
@@ -4,6 +4,12 @@
     {
         public static void Main(string[] args)
         {
+            Train train = new Train(12, 3453, 45, "se", "se", "de", 13, 324);
+            train.Move();
+            Express express = new Express(12, 3453, 45, "se", "se", "de", 13, 324, "fr");
+            express.Move();
+            Car car = new Car(12, 3453, 45, "se", "se", "de", "hb", "md");
+            car.Move();
         }
     }
 }
---
+++ Lab-4_Ex-1_var-11_Csharp/Vehicle.cs
@@ -0,0 +1,84 @@
+﻿using System;
+
+namespace Lab_4_Ex_1_var_11_Csharp
+{
+    public interface IMovable
+    {
+        void Move();
+    }
+    
+    public abstract class Vehicle : IMovable
+    {
+        protected double Speed;
+        protected double Mass;
+        protected int PassengerSeatCount;
+        protected string Name;
+        protected string EngineName;
+        protected string EngineType;
+
+        public Vehicle(double speed, double mass, int passengerSeatCount, string name, string engineName, string engineType)
+        {
+            this.Speed = speed;
+            this.Mass = mass;
+            this.PassengerSeatCount = passengerSeatCount;
+            this.Name = name;
+            this.EngineName = engineName;
+            this.EngineType = engineType;
+        }
+
+        public abstract void Move();
+    }
+
+    public class Car : Vehicle
+    {
+        private string bodyType;
+        private string motorPosition;
+
+        public Car(double speed, double mass, int passengerSeatCount, string name, string engineName, string engineType,
+            string bodyType, string motorPosition) :
+            base(speed, mass, passengerSeatCount, name, engineName, engineType)
+        {
+            this.bodyType = bodyType;
+            this.motorPosition = motorPosition;
+        }
+
+
+        public override void Move()
+        {
+            Console.WriteLine("Машина їде по дорозі\n");
+        }
+    }
+
+    public class Train : Vehicle
+    {
+        protected double ticketCost;
+        protected int carloadCount;
+
+        public Train(double speed, double mass, int passengerSeatCount, string name, string engineName,
+            string engineType,
+            double ticketCost, int carloadCount) :
+            base(speed, mass, passengerSeatCount, name, engineName, engineType)
+        {
+            this.ticketCost = ticketCost;
+            this.carloadCount = carloadCount;
+        }
+        
+        public override void Move()
+        {
+            Console.WriteLine("Поїзд їде по рельсам\n");
+        }
+    }
+
+    public class Express : Train
+    {
+        private string artificialIntelligenceNumber;
+
+        public Express(double speed, double mass, int passengerSeatCount, string name, string engineName,
+            string engineType, double ticketCost, int carloadCount,
+            string artificialIntelligenceNumber) :
+            base(speed, mass, passengerSeatCount, name, engineName, engineType, ticketCost, carloadCount)
+        {
+            this.artificialIntelligenceNumber = artificialIntelligenceNumber;
+        }
+    }
+}
